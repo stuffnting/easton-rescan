184ead90001c4eb806fce8ae7ce575416d0c0c10
stuffnting <richard@stuffnting.com>
2025-11-24 21:27:21 +0000

    initial commit with Antwerp

---
+++ eleventy.config.js
@@ -0,0 +1,76 @@
+import path from 'node:path';
+import * as sass from 'sass';
+import { HtmlBasePlugin } from '@11ty/eleventy';
+
+const lightboxPath = './node_modules/lightbox2/dist/';
+const lightboxImgPath = lightboxPath + 'images';
+
+export default function (eleventyConfig) {
+  /**
+   * Global data
+   */
+  eleventyConfig.addGlobalData('layout', 'template.njk');
+  eleventyConfig.addGlobalData('lightboxPath', lightboxPath);
+  eleventyConfig.addGlobalData('thisYear', () => new Date().getFullYear());
+
+  /**
+   * Layout alias
+   */
+  eleventyConfig.addLayoutAlias('gallery', 'thumb-gallery.njk');
+
+  /**
+   * Copy
+   */
+  eleventyConfig.addPassthroughCopy('**/large');
+  eleventyConfig.addPassthroughCopy('**/thumbs');
+  // Lightbox button images
+  eleventyConfig.addPassthroughCopy({
+    [lightboxImgPath]: 'images',
+  });
+
+  /**
+   * Path prefix. Uses pathPrefix, see returned object below.
+   */
+  eleventyConfig.addPlugin(HtmlBasePlugin);
+
+  /**
+   * Add SCSS
+   * @link https://www.11ty.dev/docs/languages/sass/
+   */
+  eleventyConfig.addTemplateFormats('scss');
+  eleventyConfig.addExtension('scss', {
+    outputFileExtension: 'css',
+    useLayouts: false,
+
+    compile: async function (inputContent, inputPath) {
+      let parsed = path.parse(inputPath);
+
+      if (parsed.name.startsWith('_')) {
+        return;
+      }
+
+      let result = sass.compileString(inputContent, {
+        loadPaths: [parsed.dir || '.', this.config.dir.includes],
+      });
+
+      this.addDependencies(inputPath, result.loadedUrls);
+
+      return async (data) => {
+        return result.css;
+      };
+    },
+  });
+
+  /**
+   * Settings
+   */
+  return {
+    dir: {
+      input: '_src',
+      output: '_dist',
+    },
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    pathPrefix: '/easton-rescan/',
+  };
+}
